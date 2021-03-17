5d84a0d23f4cbd6ac0f8aa6eaddb90041306cb8a
Nayemul <nayemulhasan2016@gmail.com>
2021-03-18 02:16:46 +0600

    private route and redirect after loggin

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,10 +3,14 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import firebaseConfig from './firebase.config';
 import { UserContext } from '../../App';
+import { useHistory, useLocation } from 'react-router';
 
 
 const Login = () => {
     const [loggedInUser, setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+    const { from } = location.state || { from: { pathname: "/" } };
    if(firebase.apps.length === 0){
     firebase.initializeApp(firebaseConfig);
    }
@@ -18,13 +22,13 @@ const Login = () => {
             const {displayName, email} = result.user;
             const loggedInUser = {name: displayName, email};
             setLoggedInUser(loggedInUser);
+            history.replace(from);
             console.log(loggedInUser);
             //console.log(result.user);
         }).catch((error) => {
             var errorCode = error.code;
             var errorMessage = error.message;
-            var email = error.email;
-            var credential = error.credential;
+            console.error(errorMessage,errorCode);
         });
     }
     return (
